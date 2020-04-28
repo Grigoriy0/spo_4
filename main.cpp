604f22722eaad5b4d6f2e6714e6df32572a496c1
Grigoriy0 <grisha.konoplich0@gmail.com>
2020-04-28 11:00:32 +0300

    windows version with mutex ready

--- main.cpp
+++ main.cpp
@@ -3,61 +3,54 @@
 #include<iostream>
 #include<conio.h>
 
-#include"mymutex.h"
+#include"mythread.h"
 
 #define MAX_THREADS 30
 
 
 namespace vars {
 	static int count = 0;
-
 	static HANDLE time_to_end_handle; // in enabled state while user don't press 'q'
-	static CRITICAL_SECTION criticalSection;
+	static Mutex mutex = Mutex("SPO_4_MUTEX");
 	static std::vector<void*> threads;
 }
-
-class CritSLocker {
-public:
-	CritSLocker(CRITICAL_SECTION *critSec) { this->critSec = critSec; while(!TryEnterCriticalSection(critSec)); }
-	~CritSLocker() { LeaveCriticalSection(critSec); }
-private:
-	CRITICAL_SECTION * critSec;
-};
 
 
 void thread_function(int uniqueNum) {
 	std::string message = std::to_string(uniqueNum) + " Hello from thread " + std::to_string(GetCurrentThreadId());
 	while (true) {
-		CritSLocker locker(&vars::criticalSection);
-		if (WaitForSingleObject(vars::time_to_end_handle, 0) == WAIT_TIMEOUT) { // user said "It's time to finish"
-			break;
+		{
+			MutexLocker locker(&vars::mutex);
+			if (WaitForSingleObject(vars::time_to_end_handle, 0) == WAIT_TIMEOUT) { // user said "It's time to finish"
+				break;
+			}
+			putchar('\n');
+			for (unsigned i = 0; i < message.size(); ++i) {
+				putchar(message[i]);
+				Sleep(100);
+			}
 		}
-		putchar('\n');
-		for (unsigned i = 0; i < message.size(); ++i){
-			putchar(message[i]);
-			Sleep(100);
-		}
+		Sleep(20);
 	}
 }
 
 void createThread() {
-	if (vars::threads.size() != MAX_THREADS) {
+	if (vars::count != MAX_THREADS) {
 		Thread<void, int> *new_thread = new Thread<void, int>(thread_function, ++vars::count);
 		vars::threads.emplace_back(new_thread);
 		new_thread->start();
 	}
+	if (vars::threads.size() == 1)
+		vars::mutex.release();
 }
 
 
 void removeThread() {
 	if (vars::threads.empty())
 		return;
-	CritSLocker locker(&vars::criticalSection);
 	((Thread<void, int>*)vars::threads.back())->cancel();
 	delete ((Thread<void, int>*)vars::threads.back());
 	vars::threads.pop_back();
-	printf("Deleted\n");
-	putchar('\n');
 }
 
 void foo(int, double){}
@@ -94,7 +87,6 @@ int main() {
 	vars::time_to_end_handle = CreateEvent(NULL, true, true, "WorkEvent");
 	if (vars::time_to_end_handle == NULL)
 		print_error("Warning! threads will be closed by hard way ");
-	InitializeCriticalSection(&(vars::criticalSection));
 	
 	while (checkInput());
 	
@@ -103,9 +95,9 @@ int main() {
 	for (void* ptr : vars::threads) {
 		Thread<void, int> *th = (Thread<void, int>*)ptr;
 		if (!th->isFinished())// if WorkEvent works, then some threads may be finished
-			th->cancel();
+			th->join();
 	}
-	DeleteCriticalSection(&vars::criticalSection);
+
 	printf("\nAll threads was canceled\n");
 	return 0;
 }
